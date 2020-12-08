0dfbfa149ba5b90470225c4050bd2d325f2beae3
khurtado801 <null801@pm.me>
2020-12-08 13:26:19 -0700

    Replaced Brand to Social Network in header.blade.php.

--- laravel/resources/views/includes/header.blade.php
+++ laravel/resources/views/includes/header.blade.php
@@ -9,7 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{ route('dashboard') }}">Brand</a>
+                <a class="navbar-brand" href="{{ route('dashboard') }}">Social Network</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
